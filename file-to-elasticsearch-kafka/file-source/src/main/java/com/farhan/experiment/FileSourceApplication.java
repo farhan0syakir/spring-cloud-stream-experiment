cd5c26a93fcaca832447efcc95db0fcbe4d0daed
farhan.syakir <farhan0syakir@gmail.com>
2023-06-10 12:07:26 +0300

    create the app

---
+++ file-to-elasticsearch-kafka/file-source/src/main/java/com/farhan/experiment/FileSourceApplication.java
@@ -0,0 +1,15 @@
+package com.farhan.experiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.fn.supplier.file.FileSupplierConfiguration;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import(FileSupplierConfiguration.class)
+public class FileSourceApplication {
+
+	public static void main(String[] args){
+		SpringApplication.run(FileSourceApplication.class, args);
+	}
+}
